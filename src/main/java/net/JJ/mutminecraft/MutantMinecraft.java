1afdc3e0186ed8f78a29428665d6fcde8f32998b
janag162 <jana.sramkova@student.gyarab.cz>
2024-03-17 16:53:09 +0100

    Implementing Geckolib

--- src/main/java/net/JJ/mutminecraft/MutantMinecraft.java
+++ src/main/java/net/JJ/mutminecraft/MutantMinecraft.java
@@ -30,6 +30,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import software.bernie.geckolib.GeckoLib;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(MutantMinecraft.MOD_ID)
@@ -46,10 +47,10 @@ public class MutantMinecraft {
 
         ModItems.register((modEventBus));
 
+        GeckoLib.initialize();
+
         modEventBus.addListener(this::commonSetup);
-
         MinecraftForge.EVENT_BUS.register(this);
-
         modEventBus.addListener(this::addCreative);
     }
 
